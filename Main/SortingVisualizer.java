dc374e61d1e267755762378602e092b0d8cdc9cc
Mario Arriaga <marriaga@csustan.edu>
2021-05-19 20:50:31 -0700

    Added save method to driver.

--- Main/SortingVisualizer.java
+++ Main/SortingVisualizer.java
@@ -1,14 +1,22 @@
+//Most of the code is not our original work
+//Any code added will be signed by the contributor who did the work
+
 package Main;
 
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import javax.imageio.ImageIO;
+
 
 import SortingMethods.*;
 
 public class SortingVisualizer {
 	
 	private static Thread sortingThread;
-
+	private static Thread sortingThread2;//Added sortingThread2 to process different start/stop calls(Waleed)
 	public static VisualizerElements frame;
 	public static Integer[] toBeSorted;
 	public static boolean isSorting = false;
@@ -49,22 +57,23 @@ public class SortingVisualizer {
 		frame.preDrawArray2(toBeSorted);//Inclusion of second array (Waleed)
 	}
 	
-	public static void startSort(String type){
-		
-		if (sortingThread == null || !isSorting){
+	public static void startSort(String type, String type2){
+		//Added String type2 to accept user choice for both arrays(Waleed)
+		//Inclusion of sortingThread2
+		if (sortingThread == null || sortingThread2 == null || !isSorting){
 			
 			resetArray();
 			
 			isSorting = true;
 
+			//Reads user's first choice
 			switch(type){
 			case "Bubble":
-				sortingThread = new Thread(new BubbleSort(toBeSorted, frame, false));
-				
+				sortingThread = new Thread(new BubbleSort(toBeSorted, frame, true));
 				break;
 				
 			case "Insertion":
-				sortingThread = new Thread(new InsertionSort(toBeSorted, frame, false));
+				sortingThread = new Thread(new InsertionSort(toBeSorted, frame, true));
 				break;
 				
 			case "Merge":
@@ -72,7 +81,7 @@ public class SortingVisualizer {
 				break;	
 				
 			case "Selection":
-				sortingThread = new Thread(new SelectionSort(toBeSorted, frame, false));
+				sortingThread = new Thread(new SelectionSort(toBeSorted, frame, true));
 				break;
 			
 			default:
@@ -80,57 +89,68 @@ public class SortingVisualizer {
 				return;
 			}
 			
-			sortingThread.start();}
+			sortingThread.start();
+
+			//Reads user's second choice
+			switch(type2){
+			case "Bubble":
+				sortingThread2 = new Thread(new BubbleSort(toBeSorted, frame, false));
+				break;
+				
+			case "Insertion":
+				sortingThread2 = new Thread(new InsertionSort(toBeSorted, frame, false));
+				break;
+				
+			case "Merge":
+				sortingThread2 = new Thread(new MergeSort());
+				break;	
+				
+			case "Selection":
+				sortingThread2 = new Thread(new SelectionSort(toBeSorted, frame, false));
+				break;
+				
+			default:
+				isSorting = false;
+				return;
+			}
+			
+			sortingThread2.start();
 			
 		}
 		
+	}
+		
+
+
+	//int x = 1 serves as a secondary condition to pass(Waleed)
+	//The two conditions determine whether the sorting process will pause or continue
+	static int x = 1;
+	@SuppressWarnings("removal")
+	public static void stopSort(Boolean pause) {
+		if (pause == true && x == 1) {
+			sortingThread.suspend();
+			sortingThread2.suspend();
+			x++;
+		}
+		else {
+			sortingThread.resume();
+			sortingThread2.resume();
+			x--;
+		}
+		
+	}
 	
-	                 /*startSort2 switch case for second array
-	                  * uses sort methods 2 to perform reDrawArray2.
-	                  * (Mario)
-	                  */
-		public static void startSort2(String s2){
-			
-			if (sortingThread == null || !isSorting){
-				
-				resetArray();
-				
-				isSorting = true;
 
-				switch(s2){
-				
-				case "Bubble":
-					sortingThread = new Thread(new BubbleSort2(toBeSorted, frame, false));
-					break;
-					
-				case "Insertion":
-					sortingThread = new Thread(new InsertionSort2(toBeSorted, frame, false));
-					break;
-					
-				case "Merge":
-					sortingThread = new Thread(new MergeSort2());
-					break;	
-					
-				case "Selection":
-					sortingThread = new Thread(new SelectionSort2(toBeSorted, frame, false));
-					break;
-				default:
-					isSorting = false;
-					return;
-				}
-				
-				sortingThread.start();
-				
-			}
-	}
-
-	//Stops the sorting algorithm(Waleed)
-	@SuppressWarnings("deprecation")
-	public static void stopSort(String selectedItem) {
+     //Captures Image of Jframe to save to file. (Mario)
+	public static BufferedImage SaveImage(Component component) {
+		BufferedImage image = new BufferedImage(component.getWidth(), component.getHeight(), BufferedImage.TYPE_INT_RGB);
+		component.paint(image.getGraphics());
+		return image;}
+	
+	//Calls on SaveImage method.
+	protected static void SaveScreenShot(Component component, String filename)  throws Exception {
 		
-		sortingThread.stop();
-		sortingThread = null;
-		
+		BufferedImage image = SaveImage(frame);
+		ImageIO.write(image,"jpeg", new File (filename));
 	}
-
 }
